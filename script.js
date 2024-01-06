8870803c9cf421e517782bee928197ab3316f289
Vanshaj Tiwari <90194253+VanshajTiwari@users.noreply.github.com>
2024-01-06 12:01:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function calculate() {
     let a = document.getElementById("Age").value;
-    a = 2022 - a;
+    a = 2024 - a;
     document.getElementById("result").style.display = "block";
     document.getElementById("result").style.animation = "appear 1s "
     document.getElementById("get").innerHTML = a;
